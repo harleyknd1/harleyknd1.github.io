e63812bcc406e677d1630d7e8b25db07276b4a8b
Aneko <thgcboss@gmail.com>
2016-12-25 19:53:14 +0100

    test
    test

---
+++ christmas/detail.js
@@ -0,0 +1,12 @@
+var env = document.querySelectorAll('.env');
+var envelope = document.querySelector('.envelope');
+
+function addclass(){
+  envelope.style.pointerEvents= 'none';
+  
+  for (var i = 0; i < env.length; i++) {
+    env[i].classList.add("clicked");
+  }
+}
+
+envelope.addEventListener('click', addclass, false);
